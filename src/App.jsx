731af402d5a50a6ad82fe63dd78295057cffc07d
Alex Caron <alex.caron@gmail.com>
2024-04-27 15:26:52 -0500

    display 13 hands

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Hand from './Hand.jsx';
 
 export default function App() {
 
-  // const numOfHands = 13;
-  let rounds = [1,2,3,4,5];
-  // for (let i=1; i<=numOfHands; i++) {
-  //   rounds.push['here'];
-  // };
+  const numOfHands = 13;
+  let rounds = [];
+  for (let i=1; i<=numOfHands; i++) {
+    rounds.push(i);
+  };
+
   return(
-    <div class="hands">
+    <div className="all-hands">
       {rounds.map((round) => (
           <Hand round={round} />
       ))}
