80037fa65dc859fa7454fe4cd8f15d1b87acf496
Alex Caron <alex.caron@gmail.com>
2024-04-27 15:09:51 -0500

    get hand div to show

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@
 import Hand from './Hand.jsx';
 
 export default function App() {
-  return (
-    <Hand round={5} />
-  );
+
+  // const numOfHands = 13;
+  let rounds = [1,2,3,4,5];
+  // for (let i=1; i<=numOfHands; i++) {
+  //   rounds.push['here'];
+  // };
+  return(
+    <div class="hands">
+      {rounds.map((round) => (
+          <Hand round={round} />
+      ))}
+    </div>
+  )
 }
